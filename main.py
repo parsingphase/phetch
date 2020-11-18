c68095c0b5fffbb32825b9de0e125bc42bf581bd
Richard George <richard@phase.org>
2020-11-18 07:31:17 -0500

    Download of hardcoded album

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import flickrapi
 from yaml import BaseLoader, load as yload
 from pathlib import Path
+import requests
+from time import sleep
 
+
+album_id = '72157714145527936'
+output_dir = './download'
 
 # config eg: {'api_key': 'MYKEY', 'api_secret': 'MYSECRET'}
 
@@ -17,8 +22,6 @@ config = yload(configData, Loader=BaseLoader)
 
 flickr = flickrapi.FlickrAPI(config['api_key'], config['api_secret'], format='parsed-json')
 
-album_id = '72157714145527936'
-
 # test = flickr.test.echo(foo="bar")
 
 # photos = flickr.photos.search(user_id='73509078@N00', per_page='10')
@@ -26,7 +29,23 @@ album_id = '72157714145527936'
 # print(photos)
 
 album = flickr.photosets.getInfo(photoset_id=album_id)
-print(album)
+# print(album)
 
-photos = flickr.photosets.getPhotos(photoset_id=album_id, extras="url_o")
-print(photos)
+photoset_response = flickr.photosets.getPhotos(photoset_id=album_id, extras="url_o")
+# urls = [photo['url_o'] for photo in photoset_response['photoset']['photo']]
+# print(urls)
+#
+# print (photoset_response)
+i = 1
+
+for photo in photoset_response['photoset']['photo']:
+    outfile = f'{output_dir}/{photo["id"]}.jpg'
+    if not Path(outfile).exists():
+        print(f'Downloading: {i}: {photo["url_o"]}:')
+        r = requests.get(photo["url_o"])
+        open(outfile, 'wb').write(r.content)
+        print(' Done')
+        sleep(2)
+        i += 1
+
+print("\nAll done")
