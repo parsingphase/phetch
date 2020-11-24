d06ae2eeebb74ac7e6f0d47a658d4bae208cb9f4
Richard George <richard@phase.org>
2020-11-23 21:00:23 -0500

    Refactor & clean up watermarker

--- watermarker.py
+++ watermarker.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 from PIL import Image
 from PIL.ImageFile import ImageFile
 
@@ -22,45 +24,64 @@ class Watermarker:
     def set_watermark_opacity(self, opacity: float):
         self.watermark_opacity = opacity
 
-    def mark_in_place(self, dest_file_path: str) -> None:
+    def mark_in_place(self, image_file_path: str) -> None:
         """
-        Apply the loaded watermark to the specified image
+        Apply the loaded watermark to the specified image and save it
 
-        :param dest_file_path:
+        :param image_file_path:
         :return:
         """
-        dest_file: ImageFile = Image.open(dest_file_path)
-        width = dest_file.width
-        height = dest_file.height
+        image: ImageFile = Image.open(image_file_path)
+        image_file = self.watermark_image(image)
+        image_file.save(image_file_path)
 
-        is_tall = height > width
+    def watermark_image(self, image: ImageFile) -> ImageFile:
+        """
+        Add the watermark to the loaded image object
+
+        :param image:
+        :return:
+        """
+        watermark_width, watermark_height = self.calculate_watermark_dimensions(image)
+        watermark_position = (image.width - watermark_width, image.height - watermark_height)
+        local_watermark = self.prepare_pastable_watermark(watermark_width, watermark_height)
+        image = self.apply_prepared_watermark(image, local_watermark, watermark_position)
+        return image
+
+    @staticmethod
+    def apply_prepared_watermark(image, local_watermark, watermark_position) -> ImageFile:
+        """
+        Apply a prepared watermark to an Image
+        :param image:
+        :param local_watermark:
+        :param watermark_position:
+        :return:
+        """
+        image = image.convert('RGBA')  # must convert to RGBA to merge with watermark
+        image.paste(local_watermark, watermark_position, local_watermark)
+        image = image.convert('RGB')  # must convert back to RGB to save jpg
+        return image
+
+    def calculate_watermark_dimensions(self, image_file: ImageFile) -> Tuple[int, int]:
+        """
+        Calculate the size a watermark should be for a given Image object such that the
+        short side of the watermark is self.short_edge_watermark_ratio of the larger image
+        :param image_file:
+        :return:
+        """
+        is_tall = image_file.height > image_file.width
         watermark_aspect = self.watermark.width / self.watermark.height
         if is_tall:
-            watermark_width = int(width * self.short_edge_watermark_ratio)
+            watermark_width = int(image_file.width * self.short_edge_watermark_ratio)
             watermark_height = int(watermark_width / watermark_aspect)
         else:
-            watermark_height = int(height * self.short_edge_watermark_ratio)
+            watermark_height = int(image_file.height * self.short_edge_watermark_ratio)
             watermark_width = int(watermark_height * watermark_aspect)
-
-        position = (width - watermark_width, height - watermark_height)
-
-        # print(self.short_edge_watermark_ratio)
-        # print(watermark_width)
-        # print(watermark_height)
-        # print(position)
-
-        local_watermark = self.prepare_pastable_watermark(watermark_width, watermark_height)
-
-        transparent = Image.new('RGBA', (width, height), (0, 0, 0, 0))
-        transparent.paste(dest_file, (0, 0))
-        transparent.paste(local_watermark, position, local_watermark)
-
-        out_file = transparent.convert('RGB')
-        out_file.save('wm_' + dest_file_path)
+        return watermark_width, watermark_height
 
     def prepare_pastable_watermark(self, watermark_width: int, watermark_height: int) -> Image:
         """
-        Scale and apply opactiy to watermark as required for use at specified size
+        Scale and apply opacity to watermark as required for use at specified size
         :param watermark_width:
         :param watermark_height:
         :return:
