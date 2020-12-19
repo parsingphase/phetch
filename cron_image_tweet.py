0348e97c3898794ce32a669dc0817213054508e5
Richard George <richard@phase.org>
2020-12-18 22:04:08 -0500

    Working twitter posting

--- cron_image_tweet.py
+++ cron_image_tweet.py
@@ -2,17 +2,23 @@
 
 import argparse
 import re
+import sys
 from datetime import datetime
 from pathlib import Path
 from typing import Generator, List, Optional, TypedDict
 
 import pendulum
+import twitter
 from dateutil.parser import parse
 
 from phetch import init_flickr_client
+from pictools import load_config
 
 ScheduledId = TypedDict('ScheduledId', {'photo_id': str, 'date_str': str})
 SimpleTweet = TypedDict('SimpleTweet', {'text': str, 'media': str})
+
+# HASHTAG = '#birdoftheday'
+HASHTAG = '#test'
 
 
 class UniquenessError(Exception):
@@ -35,10 +41,8 @@ def scan_path_for_coded_filenames(source_dir: Path) -> List[ScheduledId]:
     candidates: Generator[Path] = source_dir.glob("*.jpg")
     schedule: List[ScheduledId] = []
     for candidate in candidates:
-        # print(candidate.name)
         match = re.match(pattern, candidate.name)
         if match:
-            # print(match.groupdict())
             schedule.append(match.groupdict())
 
     return schedule
@@ -82,11 +86,10 @@ def build_tweet_by_flickr_photo_id(photo_id: str) -> SimpleTweet:
     info = flickr.photos.getInfo(photo_id=photo_id)
     when = info['photo']['dates']['taken']
     title = info['photo']['title']['_content']
-    # print(when, title)
     when_date = parse(when)
     friendly_date = pendulum.instance(when_date).format('Mo MMMM Y')
 
-    text = f'{title}, {friendly_date} #birdoftheday'
+    text = f'{title}, {friendly_date} {HASHTAG}'
 
     return {
         'text': text,
@@ -96,13 +99,20 @@ def build_tweet_by_flickr_photo_id(photo_id: str) -> SimpleTweet:
 
 def get_photo_url(flickr, photo_id):
     size_response = flickr.photos.getSizes(photo_id=photo_id)
-    print(photo_id)
-    # print(sizes)
     k_urls = [size['source'] for size in size_response['sizes']['size'] if size['label'] == 'Large 2048']
     o_urls = [size['source'] for size in size_response['sizes']['size'] if size['label'] == 'Original']
-    print(k_urls)
     url = k_urls[0] if len(k_urls) else o_urls[0]
     return url
+
+
+def init_twitter_client(config_file: str) -> twitter.Api:
+    config = load_config(config_file)['twitter']
+    api = twitter.Api(consumer_key=config['api_key'],
+                      consumer_secret=config['api_secret'],
+                      access_token_key=config['access_token_key'],
+                      access_token_secret=config['access_token_secret'])
+
+    return api
 
 
 def run_cli() -> None:
@@ -113,13 +123,20 @@ def run_cli() -> None:
 
     schedule = scan_path_for_coded_filenames(source_dir)
 
-    # print(schedule)
     assert_schedule_unique(schedule)
     due_photo = get_due_item_from_schedule(schedule)
 
     if due_photo:
         tweet: SimpleTweet = build_tweet_by_flickr_photo_id(due_photo['photo_id'])
-        print(tweet)
+        twitter_api = init_twitter_client('./config.yml')
+        status = twitter_api.PostUpdate(status=tweet['text'], media=tweet['media'])
+        if 'created_at' in status:
+            print(f"Posted successfully at {status['created_at']}")
+            print(status['media']['0']['expanded_url'])
+        else:
+            print("Possible post failure?")
+            print(status)
+            sys.exit(1)
     else:
         print("No tweet due today")
 
