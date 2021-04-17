ba17f68dc7410f54b1c7532b6f7a8effc99935c0
Richard George <richard@phase.org>
2021-04-16 22:13:26 -0400

    Refactor to aid checking of changes and avoid excess saves

--- introspect.py
+++ introspect.py
@@ -6,7 +6,7 @@ Improve keywords and title organization for all images in a folder for upload to
 import argparse
 import re
 from pathlib import Path
-from typing import Dict, List, Tuple, cast
+from typing import Dict, Optional, Set, Tuple, cast
 
 import pyexiv2
 
@@ -16,6 +16,7 @@ IPTC_KEY_SUBJECT = 'Iptc.Application2.ObjectName'
 IPTC_KEY_KEYWORDS = 'Iptc.Application2.Keywords'
 EXIF_KEY_LATITUDE = 'Exif.GPSInfo.GPSLatitude'
 EXIF_KEY_LONGITUDE = 'Exif.GPSInfo.GPSLongitude'
+GPS_LOCATION_KEYWORD = 'Approximate GPS location'
 
 
 def parse_cli_args() -> argparse.Namespace:
@@ -96,15 +97,14 @@ def degrees_float_to_dms_rational_string(degrees: float):
     return f'{int_degrees}/1 {numerator}/{denominator} 0/1'
 
 
-def round_gps_location(image, gps_dp: int) -> Dict:
+def round_gps_location(exif, gps_dp: int) -> Dict:
     """
     Take the GPS EXIF data from the supplied image and rounds its lat/long to the specified number of decimal points
-    :param image:
+    :param exif:
     :param gps_dp:
     :return:
     """
     revised_location = {}
-    exif = image.read_exif()
     if EXIF_KEY_LATITUDE not in exif or EXIF_KEY_LATITUDE not in exif:
         return {}
 
@@ -133,18 +133,19 @@ def round_dms_as_decimal(dms: str, gps_dp: int) -> str:
     return new_lat_string
 
 
-def extract_iptc_keywords(iptc: Dict) -> List[str]:
+def extract_iptc_keywords(iptc: Dict) -> Set[str]:
     """
     Get keywords from IPTC data as a list
     :param iptc:
     :return:
     """
-    keywords = []
+    keywords = ()
     if IPTC_KEY_KEYWORDS in iptc:
         keywords = iptc[IPTC_KEY_KEYWORDS]
         if not isinstance(keywords, list):
             keywords = [keywords]
-    return keywords
+
+    return set(keywords)
 
 
 def extract_image_id_from_filename(basename: str) -> str:
@@ -155,6 +156,75 @@ def extract_image_id_from_filename(basename: str) -> str:
     """
     image_id = re.sub(r'[^\d]|(\(.*)', '', basename)
     return image_id
+
+
+def populated_keys_changed(original: Dict, revised: Dict) -> bool:
+    """
+    Check whether any of the keys in revised changed from original
+    :param original:
+    :param revised:
+    :return:
+    """
+    changed = False
+
+    for (key, value) in revised.items():
+        if key in original:
+            if type(value) == list:
+                changed = len(set(value) - set(original[key])) > 0
+            else:
+                changed = original[key] != revised[key]
+        else:
+            changed = True
+            break
+
+    return changed
+
+
+def revise_iptc(iptc, additional_keywords: Optional[Set] = None) -> Dict:
+    """
+    Regenerated IPTC data based on keywords and subject
+    additional_keywords will not be considered as candidates for subject
+    :param iptc:
+    :param additional_keywords:
+    :return:
+    """
+    if additional_keywords is None:
+        additional_keywords = ()
+
+    revised_iptc = {}
+
+    # Update image properties to/from keywords
+    keywords = extract_iptc_keywords(iptc)
+    non_machine_keywords = [k for k in keywords if ':' not in k and k != GPS_LOCATION_KEYWORD]
+    if IPTC_KEY_SUBJECT not in iptc:
+        longest_keyword = max(non_machine_keywords, key=len)
+        if longest_keyword:
+            subject = make_subject(longest_keyword)
+            revised_iptc[IPTC_KEY_SUBJECT] = subject
+
+    keywords = keywords.union(set(additional_keywords))
+
+    if len(keywords) > 0:
+        revised_iptc[IPTC_KEY_KEYWORDS] = list(keywords)
+
+    return revised_iptc
+
+
+def save_revised_image(image, basename: str, revised_exif: Dict, revised_iptc: Dict):
+    """
+    Store any changed data to the image at the provided location
+    :param image:
+    :param basename:
+    :param revised_exif:
+    :param revised_iptc:
+    :return:
+    """
+    if len(revised_iptc.keys()) > 0:
+        image.modify_iptc(revised_iptc)
+        print(f'Revised IPTC for {basename}', revised_iptc)
+    if len(revised_exif.keys()) > 0:
+        image.modify_exif(revised_exif)
+        print(f'Revised EXIF for {basename}', revised_exif)
 
 
 def run_cli() -> None:
@@ -169,66 +239,38 @@ def run_cli() -> None:
         filename = str(source_file)
         image = pyexiv2.Image(filename)
 
+        exif = image.read_exif()
+        revised_exif = {}
+
         # Revise location if specified
-        geo_exif = {}
         if args.gps_dp is not None:
-            geo_exif = round_gps_location(image, args.gps_dp)
-            if len(geo_exif.keys()) == 0:
-                print(f'No geo data in {filename}')
+            revised_exif = round_gps_location(exif, args.gps_dp)
 
-        # Update image properties to/from keywords
+        # Now gather the keywords implied by the image location and data
         image_id = extract_image_id_from_filename(basename)
+        keywords = [f'library:fileId={image_id}']
 
-        iptc = image.read_iptc()
-        revised_iptc = {}
-
-        keywords = extract_iptc_keywords(iptc)
-        keywords.append(f'library:fileId={image_id}')
-
-        non_machine_keywords = [k for k in keywords if ':' not in k]
-
-        subject = ''
-        if IPTC_KEY_SUBJECT in iptc:
-            # Leave existing subjects alone
-            subject = iptc[IPTC_KEY_SUBJECT]
-        elif non_machine_keywords:
-            longest_keyword = max(non_machine_keywords, key=len)
-            if longest_keyword:
-                revised_iptc[IPTC_KEY_SUBJECT] = subject = make_subject(longest_keyword)
-
-        # Add some informational keywords if we modified GPS
-        if len(geo_exif.keys()):
-            keywords.append('Approximate GPS location')
+        if populated_keys_changed(exif, revised_exif):
+            keywords.append(GPS_LOCATION_KEYWORD)  # human-readable
             keywords.append(f'gps:accuracy={args.gps_dp}dp')
 
-        if len(keywords) > 0:
-            revised_iptc[IPTC_KEY_KEYWORDS] = list(set(keywords))  # set removes dups
+        # Find and apply subjects and keywords
+        iptc = image.read_iptc()
+        keywords = set(keywords)  # normalize
+        revised_iptc = revise_iptc(iptc, keywords)
 
-        save_revised_image(image, basename, geo_exif, revised_iptc)
+        if populated_keys_changed(exif, revised_exif) or populated_keys_changed(iptc, revised_iptc):
+            # We don't want to update (and change timestamp) if nothing changed
+            save_revised_image(image, basename, revised_exif, revised_iptc)
         image.close()
 
         if args.rename:
+            # If we can get a subject from revised or existing data, apply it to the name
+            subject = revised_iptc.get(IPTC_KEY_SUBJECT, iptc.get(IPTC_KEY_SUBJECT, None))
             if subject and '(' not in basename:
                 new_filename = source_file.with_name(f'{source_file.stem} ({subject}){source_file.suffix}')
                 source_file.rename(new_filename)
                 print(f' Renamed {filename} to {new_filename}')
-
-
-def save_revised_image(image, basename: str, revised_exif: Dict, revised_iptc: Dict):
-    """
-    Store any changed data to the image at the provided location
-    :param image:
-    :param basename:
-    :param revised_exif:
-    :param revised_iptc:
-    :return:
-    """
-    if revised_iptc.keys():
-        image.modify_iptc(revised_iptc)
-        print(f'Revised IPTC for {basename}', revised_iptc)
-    if revised_exif.keys():
-        image.modify_exif(revised_exif)
-        print(f'Revised EXIF for {basename}', revised_exif)
 
 
 if __name__ == '__main__':
