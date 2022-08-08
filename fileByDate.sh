168fdedd6c13bb54e5ec3ca211975bf3103a791b
Richard George <richard@phase.org>
2022-08-08 17:45:28 -0400

    fileByDate enhancements

--- fileByDate.sh
+++ fileByDate.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Point to a folder containing a flat export of Lightroom originals
+
+PHOTO_DIR="/Users/wechsler/Pictures/Archive 2016-2021"
+
 
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
@@ -9,39 +13,62 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
 done
 SCRIPT_DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 
-DIR="/Users/wechsler/Pictures/Lightroom Export 2020-2021"
 
-cd "$DIR" || exit 1
+cd "$PHOTO_DIR" || exit 1
 
 function getDateDir () {
   FILE=$1
-  DIR=$(exiftool -q -q -d "%Y/%m/%d" -p '$CreateDate' $FILE)
-  echo $DIR
+  DATEDIR=$(exiftool -q -q -d "%Y/%m/%d" -p '$CreateDate' $FILE)
+  echo $DATEDIR
 }
 
-FILES=$(find . -maxdepth 1 -type f -exec echo '{}' \;)
+FILES=$(find -s . -maxdepth 1 -type f -exec echo '{}' \;)
 
+#set -x
+OLDIFS=$IFS
+IFS=$'\n'
 for FILE in $FILES:
 do
+  echo "Checking ${FILE}"
   BASEFILE=$(basename -- $FILE)
   STEM="${BASEFILE%.*}"
   EXT="${BASEFILE##*.}"
   UEXT=$(echo $EXT | tr '[:lower:]' '[:upper:]')
+
+  if [[ "$UEXT" == "DNG" ]]; then
+    echo "    Skip DNG:${BASEFILE}"
+    continue
+  fi
+
   if [[ "$UEXT" == "XMP" ]]; then
-    echo "Skip XMP:${BASEFILE}"
+    echo "    Skip XMP:${BASEFILE}"
     continue
   fi
 
   NEWDIR=$(getDateDir $FILE)
+#  echo $NEWDIR
+  if [[ "$NEWDIR" == "" ]]; then
+    echo "No data for ${FILE}"
+    continue
+  fi
+
   mkdir -p $NEWDIR
   echo "$BASEFILE : $UEXT => $NEWDIR"
+
+  # shouldn't run context script if we don't copy…
+  if [[ -f "$NEWDIR/$BASEFILE" ]]; then
+    echo "$NEWDIR/$BASEFILE already exists"
+  else
+    . "${SCRIPT_DIR}/context.sh" "$BASEFILE" -k >> "$NEWDIR/catalog.txt"
+    mv -n "$BASEFILE" "${NEWDIR}"
+  fi
+
   if [[ "$UEXT" == "CR2" || "$UEXT" == "CR3" ]]; then
     if [[ -f "$STEM.xmp" ]]; then
-      echo cp "$STEM.xmp" "${NEWDIR}"
+      echo "    $STEM.xmp => ${NEWDIR}"
+      mv -n "$STEM.xmp" "${NEWDIR}"
     fi
   fi
-  echo cp "$BASEFILE" "${NEWDIR}"
-  . "${SCRIPT_DIR}/context.sh" "$BASEFILE" -k >> "$NEWDIR/catalog.txt"
 done
-
-open .
+IFS=$OLDIFS
+#open .
